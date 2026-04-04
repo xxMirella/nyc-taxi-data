eb19c275e94ac65ee99b852d3aa2d0d85a47f04e
xxMirella <mirella.mih11006@gmail.com>
2026-04-04 17:39:13 -0300

    adjusting backend.hcl file.

--- bootstrap/deploy-role.tf
+++ bootstrap/deploy-role.tf
@@ -1,5 +1,9 @@
-locals {
-  github_sub = var.github_environment != "" ? "repo:${var.github_repo}:environment:${var.github_environment}" : "repo:${var.github_repo}:ref:refs/heads/${var.github_branch}"
+variable "github_repo" {
+  type = string
+}
+
+variable "github_oidc_subjects" {
+  type = list(string)
 }
 
 data "aws_iam_policy_document" "github_actions_assume_role" {
@@ -7,9 +11,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     sid    = "AllowGitHubActionsOIDC"
     effect = "Allow"
 
-    actions = [
-      "sts:AssumeRoleWithWebIdentity"
-    ]
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
       type        = "Federated"
@@ -23,9 +25,9 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "ForAnyValue:StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = [local.github_sub]
+      values   = var.github_oidc_subjects
     }
   }
 }
